6f33ec9a1aeb7f0fa86357251e70e29850d1b368
SergioMartinAlvaro <smaral.sergio@gmail.com>
2019-04-11 01:08:24 +0200

    Added project to repository

---
+++ src/app/pages/auth/login/login.page.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { AlertService } from './../../../services/alert.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { RegisterPage } from '../register/register.page';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private alertService: AlertService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  dimissLogin() {
+    this.modalController.dismiss();
+  }
+
+  async registerModal() {
+    this.dimissLogin();
+    const registerModal  = await this.modalController.create({
+      component: RegisterPage
+    });
+    return await registerModal.present();
+  }
+
+  login(form: NgForm) {
+    this.authService.login(form.value.email, form.value.password).subscribe(
+      data => {
+        this.alertService.presentToast("Logged In");
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        this.dimissLogin();
+        this.navCtrl.navigateRoot('/dashboard');
+      }
+    )
+  }
+
+}
---
+++ src/app/services/env.service.ts
@@ -0,0 +1,12 @@
+//In this service put the Environment variables
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvService {
+
+  API_URL = '';
+
+  constructor() { }
+}
