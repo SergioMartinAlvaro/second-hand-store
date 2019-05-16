e3a42d273bb95e984cac458117de39317df3dc9b
SergioMartinAlvaro <smaral.sergio@gmail.com>
2019-05-17 01:43:31 +0200

    Added products to category view

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,5 +1,16 @@
 export class Category {
     CategoryId: number;
     CategoryName: string;
-    products: any[];
+    CategoryImage: string;
+    CategoryDescription: string;
+    Products: any[];
+
+    constructor(CategoryId: number, CategoryName: string, CategoryDescription: string,
+        CategoryImage: string, products: any[]) {
+            this.CategoryId = CategoryId;
+            this.CategoryName = CategoryName;
+            this.CategoryDescription = CategoryDescription;
+            this.CategoryImage = CategoryImage;
+            this.Products = products;
+        }
 }
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -11,23 +11,37 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categoryId: any;
-  category: any;
+  category: Category;
+  token: any;
+  private localStorage: any;
 
   constructor( 
     private route: ActivatedRoute,
     private router: Router,
-    private _categoryService: CategoryService) { }
+    private _categoryService: CategoryService) {
+      this.localStorage = localStorage;
+     }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.categoryId = Number.parseInt(params['categoryId']);
-      console.log(this.categoryId);
    });
+
+   console.log(this.localStorage);
+   this.token = this.localStorage["token"];
+   console.log(this.token);
 
    try {
     this._categoryService.getCategory(this.categoryId).subscribe(
       data => {
-        this.category = data;
+        var selectedCategory = new Category(
+          data["categoryId"],
+          data["categoryName"],
+          data["categoryDescription"],
+          data["categoryImage"],
+          data["products"]
+        );
+        this.category = selectedCategory;
       }
     )
    } catch(e) {
