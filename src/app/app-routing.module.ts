2c8acabaa121a2983a3dd425937b3cd20caeb360
SergioMartinAlvaro <smaral.sergio@gmail.com>
2019-06-12 01:28:54 +0200

    Refactored UI design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,8 @@ const routes: Routes = [
   { path: 'create-category', loadChildren: './pages/category/create-category/create-category.module#CreateCategoryPageModule' },
   { path: 'update-category/:categoryId', loadChildren: './pages/category/update-category/update-category.module#UpdateCategoryPageModule' },
   { path: 'shopping-cart/:userId', loadChildren: './pages/shopping-cart/shopping-cart.module#ShoppingCartPageModule' },
-  { path: 'update-product/:productId', loadChildren: './pages/product/update-product/update-product.module#UpdateProductPageModule' },
+  { path: 'update-product/:productId', loadChildren: './pages/product/update-product/update-product.module#UpdateProductPageModule' },  { path: 'pay-shopping-cart', loadChildren: './pages/shopping-cart/pay-shopping-cart/pay-shopping-cart.module#PayShoppingCartPageModule' },
+
 
 
 
