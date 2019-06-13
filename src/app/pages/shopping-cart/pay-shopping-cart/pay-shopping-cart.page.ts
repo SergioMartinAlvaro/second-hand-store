c0833b818534528da5ec15d968a6f0b6c0f42314
Martin Alvaro, Sergio <SergioMar@youforce.net>
2019-06-13 10:01:32 +0200

    Changes to production environment

--- src/app/pages/shopping-cart/pay-shopping-cart/pay-shopping-cart.page.ts
+++ src/app/pages/shopping-cart/pay-shopping-cart/pay-shopping-cart.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ShoppingcartServiceService } from 'src/app/services/shoppingcart-service.service';
 import { AlertService } from 'src/app/services/alert.service';
 import { ShoppingCart } from 'src/app/models/shopping-cart';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pay-shopping-cart',
@@ -27,17 +28,22 @@ export class PayShoppingCartPage implements OnInit {
   ngOnInit() {
   }
 
-  payShoppingCart() {
-    this._shoppingCartService.payShoppingCart(this.shoppingCart.shoppingCartId, this.shoppingCart.user).subscribe(
-      data => {
-        this._shoppingCartService.createShoppingCart(this.shoppingCart.user).subscribe(
-          data => {
-            this._alertService.presentToast("Shoppnig cart succesfully paid.");
-            this.router.navigate(["dashboard"]);
-          }
-        )
-      }
-    )
+  pay(form: NgForm) {
+    if(form.value.Owner, form.value.Number, form.value.CCV) {
+      this._shoppingCartService.payShoppingCart(this.shoppingCart.shoppingCartId, this.shoppingCart.user).subscribe(
+        data => {
+          this._shoppingCartService.createShoppingCart(this.shoppingCart.user).subscribe(
+            data => {
+              this._alertService.presentToast("Shoppnig cart succesfully paid.");
+              this.router.navigate(["dashboard"]);
+            }
+          )
+        }
+      )
+    } else {
+      this._alertService.presentToast("Please, fill all the fields to pay.");
+    }
+
   }
 
 }
